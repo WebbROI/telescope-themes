b365687c330e84c5c4d108ab7820ad69e5ec3117
Val Galin <val.galin@gmail.com>
2015-06-19 10:37:46 +0800

    Update package version

--- telescope-theme-iris/package.js
+++ telescope-theme-iris/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'webbroi:telescope-theme-iris',
-  version: '0.1.0',
+  version: '0.1.1',
   summary: 'Telescope Iris theme package for RefactorScope v0.20 version.',
   git: 'https://github.com/WebbROI/telescope-themes.git'
 });
